36a6093f1157b79d91c0e1e85c33ca9eb1d92ed6
Ivan Hernandez <ihernandezgo23wg@ikzubirimanteo.com>
2025-03-11 15:31:03 +0100

    Top menu done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export const Navbar = () => {
+  const [selected, setSelected] = useState("Alojamientos");
+
+  return (
+    <header className="flex flex-wrap justify-between items-center px-5 py-3 border-b">
+      {/* Logo */}
+      <a href="/">
+        <img
+          src="src/assets/logo.png"
+          alt="airbnb logo"
+          className="h-10"
+        />
+      </a>
+
+      {/* Centro - Con selección */}
+      <div className="flex ml-10 gap-10 items-center w-full md:w-auto justify-center md:justify-start mt-3 md:mt-0">
+        <h3
+          className={`cursor-pointer ${selected === "Alojamientos" ? "text-gray-900 font-bold" : "text-gray-400"}`}
+          onClick={() => setSelected("Alojamientos")}
+        >
+          Alojamientos
+        </h3>
+        <h3
+          className={`cursor-pointer ${selected === "Experiencias" ? "text-gray-900 font-bold" : "text-gray-400"}`}
+          onClick={() => setSelected("Experiencias")}
+        >
+          Experiencias
+        </h3>
+      </div>
+
+      {/* Derecha */}
+      <div className="flex gap-5 items-center">
+        <h3>Pon tu casa en Airbnb</h3>
+        <img id="browser" src="src/assets/icons8-browser-100.png" className="h-6" />
+
+        <div className="flex gap-2 p-2 items-center border rounded-full">
+          <img id="menu" src="src/assets/icons8-menu-100.png" className="h-5" />
+          <img id="user" src="src/assets/icons8-user-60.png" className="h-6 pl-1" />
+        </div>
+      </div>
+    </header>
+  );
+};
